b90fd594e89c2d2597a2a84ed70bdb64287f473b
Enric Morales <me@enric.me>
2015-03-26 22:29:58 +0100

    Add logging and argument parsing

--- amerigo.py
+++ amerigo.py
@@ -3,9 +3,11 @@
 import socket
 import struct
 import geojson
+import argparse
+import logging as log
 
 FILE = "./position.geojson"
-UDP_IP = "0.0.0.0"
+UDP_ADDR = "0.0.0.0"
 UDP_PORT = 49000
 
 rosetta = {20: ["lat", "lon", "alt_amsl", "alt_agl",
@@ -43,25 +45,38 @@ def parse_stream(data):
                 # Append the translated 'data_set' to the output dict
                 output.update(dict(zip(rosetta[data_set], data)))
             else:
-                print("[ERROR] Couldn't parse:", data)
+                log.warning("[ERROR] Couldn't parse:", data)
 
         return output
 
 
 def main():
+    # Parse arguments
+    arg = argparse.ArgumentParser()
+    arg.add_argument("-p", "--port", type=int, default=UDP_PORT,
+                     help="port to bind to (default: %(default)s)")
+    arg.add_argument("-a", "--address", default=UDP_ADDR,
+                     help="address to bind to (default: %(default)s)")
+    arg.add_argument("-o", "--output", default=FILE,
+                     help="file that shall be written (default: %(default)s)")
+    arg.add_argument("-v", "--verbose",
+                     help="turn on verbose output")
+    args = arg.parse_args()
+
     # Bind to an internet-facing UDP socket
     udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    udp_socket.bind((UDP_IP, UDP_PORT))
+    udp_socket.bind((args.address, args.port))
+    log.info("Binding to {}:{}".format(args.address, args.port))
 
     while True:
         # Receive with a buffer of 1024 bytes
         data, addr = udp_socket.recvfrom(1024)
-        parsed = parse(data)
+        parsed = parse_stream(data)
         point = geojson.Point(parsed["lon"], parsed["lat"],
                               parsed["alt_amsl"])
         feature = geojson.Feature(geometry=point)
 
-        with open(FILE, mode="w") as f:
+        with open(args.output, mode="w") as f:
             f.write(str(feature))
 
 

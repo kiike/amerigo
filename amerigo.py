caa547a7fc27edfd01dcf0f8ce916b96fb962f45
Enric Morales <me@enric.me>
2015-04-13 08:15:23 +0200

    Add a docstring to the main program

--- amerigo.py
+++ amerigo.py
@@ -1,4 +1,29 @@
 #!/usr/bin/env python
+"""
+This program is part of amerigo, a project to replace the utilities that
+rely on Google Earth/Google Maps, and to play with building apps on top
+of the X-Plane UDP interface.
+
+Its functions are:
+    * to receive the input of X-Plane via UDP
+    * to translate the input into something we can work with
+    * to build a GeoJSON file we can render with tools such as Leaflet
+    * to start an HTTP server for easy serving of the files
+
+Copyright (c) 2015 Enric Morales <me@enric.me>
+
+Permission to use, copy, modify, and distribute this software for any
+purpose with or without fee is hereby granted, provided that the above
+copyright notice and this permission notice appear in all copies.
+
+THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+"""
 
 import socket
 import http.server
